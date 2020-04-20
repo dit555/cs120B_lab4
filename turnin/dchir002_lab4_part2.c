6ed1a2a9fb4c95c750003c5a282bd5cf0a93e6c7
dit555 <dchir002@ucr.edu>
2020-04-19 21:34:08 -0700

    2

--- turnin/dchir002_lab4_part2.c
+++ turnin/dchir002_lab4_part2.c
@@ -20,19 +20,23 @@ void Tick(){
 	switch(state){ //transitions
 		case Start: temp2 = 0x07; state = Wait; break;
 		case Wait: 
-			if (temp == 0x01)
+			if (temp == 0x01){
 			    	state = Inc;
-			else if (temp == 0x02)
+				if(temp != 9) temp2++;
+			}
+			else if (temp == 0x02){
 				state = Dec;
+				if(temp != 0) temp2--;
+			}
 			else if (temp == 0x03)
 				state = Reset;
 			else
 				state = Wait;
 			break;
 		case Inc2: state = Wait; break;
-		case Inc: state = (temp == 0x01) ? Inc : Inc2; if(temp != 9) temp2++; break;
+		case Inc: state = (temp == 0x01) ? Inc : Inc2; break;
 		case Dec2: state = Wait; break;
-		case Dec: state = (temp == 0x02) ? Dec : Dec2; if(temp != 0) temp2--; break;
+		case Dec: state = (temp == 0x02) ? Dec : Dec2; break;
 		case Reset: state = Reset2; break;
 		case Reset2: state = (temp == 0x03) ? Reset : Wait; break;
 		default: state = Start; break;
